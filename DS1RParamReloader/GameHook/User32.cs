d8bf27db52505d9f91dd2834ce17512a9c6a7569
Scott Mooney <swj.mooney@gmail.com>
2023-07-01 14:26:23 -0400

    User32 namespace

--- DS1RParamReloader/GameHook/User32.cs
+++ DS1RParamReloader/GameHook/User32.cs
@@ -1,21 +1,20 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 
-namespace DS1RParamReloader.GameHook
+namespace DS1RParamReloader.GameHook;
+
+internal static class User32
 {
-    internal static class User32
+    [DllImport("user32.dll")]
+    static extern IntPtr GetWindowThreadProcessId(IntPtr hWnd, out uint ProcessId);
+
+    [DllImport("user32.dll")]
+    static extern IntPtr GetForegroundWindow();
+
+    public static uint GetForegroundProcessID()
     {
-        [DllImport("user32.dll")]
-        static extern IntPtr GetWindowThreadProcessId(IntPtr hWnd, out uint ProcessId);
-
-        [DllImport("user32.dll")]
-        static extern IntPtr GetForegroundWindow();
-
-        public static uint GetForegroundProcessID()
-        {
-            IntPtr hWnd = GetForegroundWindow();
-            GetWindowThreadProcessId(hWnd, out uint pid);
-            return pid;
-        }
+        IntPtr hWnd = GetForegroundWindow();
+        GetWindowThreadProcessId(hWnd, out uint pid);
+        return pid;
     }
 }
